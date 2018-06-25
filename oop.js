3fbc78ea7ede81968257586d23c9032b2f22d289
CHRISTIAN EGWIM <christian.egwim@venturegardengroup.com>
2018-06-25 16:49:52 +0100

    Defining objects as either factories or constructors with their defferencies in mind

--- oop.js
+++ oop.js
@@ -3,7 +3,7 @@ console.log('Hello Oop!!!')
 // Objects
 const circle = {
     radius: 2,                 //here radius and locatio are properties.
-    locatiom: {
+    location: {
         x: 1,
         y: 2
     },
@@ -14,3 +14,34 @@ const circle = {
 
 //calling the object:
 circle.draw();
+
+//Two ways to define an object is by 1. factories and by 2. constructors:
+
+// Factory function
+function createCircle(radius){
+    return{
+        radius,
+        draw: function(){
+            console.log('draw from create circle!');
+        }
+    };
+}
+
+const myCircle = createCircle(1);
+myCircle.draw();
+
+
+// Constructor Function
+//The "This" keyword is basically a reference to the object referencing this piece of code.
+// Constructor in Js starts with Capital letter of its object.
+
+function Circle (radius){
+     this.radius = radius;
+     this.draw = function(){
+         console.log('drawing from constructor');
+     }
+}
+
+const anotherCircle = new Circle(5);
+console.log("This is to display the call to the constructor",anotherCircle);
+
