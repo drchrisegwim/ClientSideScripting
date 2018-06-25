7519ab77fbdcfb4ab829432475ef9ec3f71e1280
CHRISTIAN EGWIM <christian.egwim@venturegardengroup.com>
2018-06-25 15:43:36 +0100

    Object Literials

--- oop.js
+++ oop.js
@@ -1,3 +1,16 @@
 console.log('Hello Oop!!!')
 
+// Objects
+const circle = {
+    radius: 2,                 //here radius and locatio are properties.
+    locatiom: {
+        x: 1,
+        y: 2
+    },
+    draw: function(){          // here draw is a method
+        console.log('draw');
+    }
+};
 
+//calling the object:
+circle.draw();
